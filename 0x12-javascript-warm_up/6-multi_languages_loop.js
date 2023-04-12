6f3fc922dd9cab7a52292c7fb64b0bee53d22ccb
Victoriabunmi72 <bhummhie97@gmail.com>
2023-04-12 10:00:15 -0700

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// using array of string and a loop
+
+const value = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// looping through
+
+for (const item of value) {
+  console.log(item);
+}
