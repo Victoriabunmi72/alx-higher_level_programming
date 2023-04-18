5931ab5ed0586e85dcc3fb64af18a945dedb40bf
Victoriabunmi72 <bhummhie97@gmail.com>
2023-04-18 05:28:35 -0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,15 +18,15 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute = ("SELECT * FROM states",
-                    "WHERE name LIKE 'N%'",
-                    "ORDER BY states.id ASC")
-    # HERE I have to know SQL to grab all states in my database
+    myQuery = "".joint[
+            "SELECT * FROM states WHERE name LIKE BINARY 'N%'",
+            "ORDER BY states.id ASC")
+
+    cur.execute(myQuery)
+    curs = cur.fetchall()
     
     """print the state names, while iterating through the rows."""
-
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    for row in curs:
         print(row)
 
     cur.close()
