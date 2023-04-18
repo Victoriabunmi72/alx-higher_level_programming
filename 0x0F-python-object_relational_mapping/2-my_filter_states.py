d4d0ce64205db91ff76abb4f47ceec11ee43dd8a
Victoriabunmi72 <bhummhie97@gmail.com>
2023-04-18 06:10:21 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,20 +5,23 @@ import sys
 
 
 if __name__ == "__main__":
-    
-    conn = MySQLdb.connect(host="localhost", port=3306,
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3],
-                        charset="utf8")
+
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8")
 
     """execute the query to retrieve information about the state."""
-    query = " ".join([
+    myQuery = " ".join([
             "SELECT * FROM states",
             "WHERE name LIKE BINARY '{}'",
             "ORDER BY id ASC",
             ]).format(sys.argv[4])
-    cur.execute(query)
+
+    cur.execute(myQuery)
     cur = conn.cursor()
 
     """fetching and printing out the, result"""
