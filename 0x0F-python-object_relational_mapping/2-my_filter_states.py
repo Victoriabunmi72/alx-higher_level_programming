0f180e476d842f4fb05bab2ed7b2c551acf2db52
Victoriabunmi72 <bhummhie97@gmail.com>
2023-04-17 21:44:06 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
                         charset="utf8")
 
     """execute the query to retrieve information about the state."""
-    cur = conn.cursor()
-    cur.execute(query, (state_name,))
     query = " ".join([
             "SELECT * FROM states",
             "WHERE name LIKE BINARY '{}'",
             "ORDER BY id ASC",
             ]).format(sys.argv[4])
+    cur.execute(query)
+    cur = conn.cursor()
 
     """fetching and printing out the, result"""
     result = cur.fetchall()
