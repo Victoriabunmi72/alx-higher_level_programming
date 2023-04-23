9319efb685f3df8c22b2000c5cbb75dbc6c112f1
Victoriabunmi72 <bhummhie97@gmail.com>
2023-04-22 21:10:00 -0700

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-"""State object from the database hbtn+0e_6_usa"""
-import sys
-from sqlachemy import (create_engine)
-from sqlachemy.orm import sessionmaker
-from model_state import Base, State
+"""Print all state."""
 
-if __name__ == "
---main__":
-      eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(sys.argv[1], sys.argv[2],
-                                sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(eng)
-    session = sessionmaker(bind=eng)
-    session = session()
-    for i in session.query(State).order_by(State.id).all():
-        print(f"{i.id}: {i.name}")
-    session.close
+import sys
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    try:
+
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(sys.argv[1],
+                                       sys.argv[2],
+                                       sys.argv[3]))
+        Base.metadata.create_all(engine)
+
+        Session = sessionmaker()
+        Session.configure(bind=engine)
+        session = Session()
+        for data in session.query(State).order_by(State.id):
+            print("{}: {}".format(data.id, data.name))
+
+    except IndexError as e:
+        print("Usage:{} <user> <password> database"
+              .format(sys.argv[1]))
+        exit(-1)
