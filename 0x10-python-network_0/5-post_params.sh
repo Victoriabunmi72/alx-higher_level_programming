10e98becd83b99ddb53f673e5d64cf715bfe75d9
Victoriabunmi72 <bhummhie97@gmail.com>
2023-04-26 01:19:00 +0100

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
-#!/bin/bash/python3
-#
+#!/bin/bash
+# POST request to the passed URL, and displays the body of the response
 curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" "${1}"
