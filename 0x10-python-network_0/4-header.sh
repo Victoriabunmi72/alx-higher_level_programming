4c1364569f2f1a04e88897f5c199cf3f5eab472b
Victoriabunmi72 <bhummhie97@gmail.com>
2023-04-26 01:23:52 +0100

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A header variable X-School-User-Id must be sent with the value 98
-
+# Variable X-School-User-Id must be sent with the value 98
 curl -sH "X-School-User-Id: 98" "${1}"
