fe40de6ccce6f190d83f8ed981e6923c0043be04
Victoriabunmi72 <bhummhie97@gmail.com>
2023-04-29 04:04:35 +0100

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    ___with the email as a parameter, and finally displays the body of the response.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
