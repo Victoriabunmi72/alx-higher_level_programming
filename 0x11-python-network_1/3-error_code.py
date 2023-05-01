f23d609ea24870bb23b0b5e2abdf7d6642c95229
Victoriabunmi72 <bhummhie97@gmail.com>
2023-05-01 07:19:21 +0100

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """A python script that takes in a URL"""
 
-import sys
-from urllib.error import HTTPError
 from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+import sys
 
 if __name__ == "__main__":
     try:
         url = sys.argv[1]
         req = Request(url)
         with urlopen(req) as vic:
-            print(vic.read().decode('UTF-8'))
+            print(vic.read().decode('utf-8'))
     except HTTPError as err:
         print("Enter code:{}".format(err.getcode()))
