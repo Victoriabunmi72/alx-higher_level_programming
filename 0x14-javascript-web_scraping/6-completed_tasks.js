951f88ec879515e9b2940fa6d641b12e7e28e3b5
Victoriabunmi72 <bhummhie97@gmail.com>
2023-05-24 09:32:43 +0100

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//script that computes the number of tasks completed by user id.
+//script that computes the number of tasks completed by user
 const request = require('request');
 const url = process.argv[2];
 
