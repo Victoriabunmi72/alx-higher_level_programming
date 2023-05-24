f98deffc46f80ee18f4ff25a831b1283c3a60506
Victoriabunmi72 <bhummhie97@gmail.com>
2023-05-24 09:55:28 +0100

    100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+
+// Retrieve the movie ID from the command-line argument
+const movieId = process.argv[2];
+const url = `https://swapi.co/api/films/${movieId}`;
+
+request(url, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
+
+    characters.forEach((characterUrl) => {
+      request(characterUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          const character = JSON.parse(body);
+          const characterName = character.name;
+          console.log(characterName);
+        }
+      });
+    });
+  }
+});
