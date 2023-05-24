5e23debdd73dfecaa4aa4e6ab1b0ae1fc3aaaeab
Victoriabunmi72 <bhummhie97@gmail.com>
2023-05-24 09:26:17 +0100

    5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+//script that gets the contents of a webpage and stores it in a file.
+
+const request = require('request');
+const fs = require('fs');
+const args = process.argv.slice(2);
+
+function scrapeWebPageToFile (url, fileName) {
+  request.get(url, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      fs.writeFile(fileName, body, 'utf8', function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
+  });
+}
+
+if (args.length === 2) {
+  const url = args[0];
+  const fileName = args[1];
+  scrapeWebPageToFile(url, fileName);
+}
